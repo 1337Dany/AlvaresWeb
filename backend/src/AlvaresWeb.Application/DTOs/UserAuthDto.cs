edb625a46d7c06e17da462ecf0bcf30fae1f00b0
1337Dany <dmunatayev.dev@gmail.com>
2026-01-07 04:19:47 +0100

    added validation to models

--- backend/src/AlvaresWeb.Application/DTOs/UserAuthDto.cs
+++ backend/src/AlvaresWeb.Application/DTOs/UserAuthDto.cs
@@ -1,8 +1,15 @@
-﻿namespace AlvaresWeb.Application.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlvaresWeb.Application.DTOs;
 
 public class UserAuthDto
 {
+    [Required]
+    [MinLength(3)]
+    [RegularExpression(@"^[a-zA-Z0-9_]+$")]
     public string Username { get; set; } = null!;
     
+    [Required]
+    [MinLength(6)]
     public string Password { get; set; } = null!;
 }
